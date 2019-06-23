2958a932a314f17d0a1041ae6ab19ea7e2563d64
Aruna Gamage <admgbit@gmail.com>
2019-06-23 15:57:49 +0530

    Added User NAme And Password

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, AppRegistry, Image, TextInput, TouchableOpacity, ImageBackground} from 'react-native';
 
 const background = require("./background.jpg");
+const lockIcon = require("./lock.png");
+const personIcon = require("./person.png");
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -27,6 +29,40 @@ export default class App extends Component {
       <View style={{ flex: 1}}>
       <ImageBackground source={pic} style={[styles.background, styles.container]} resizeMode="cover">
       <Text>Inside</Text>
+
+      <View style={styles.container} />
+      <View style={styles.wrapper}>
+        <View style={styles.inputWrap}>
+        <View style={styles.iconWrap}>
+              <Image
+                source={personIcon}
+                style={styles.icon}
+                resizeMode="contain"
+              />
+            </View>
+            <TextInput
+              placeholder="Username"
+              style={styles.input}
+              underlineColorAndroid="transparent"
+            />
+        </View>
+
+        <View style={styles.inputWrap}>
+            <View style={styles.iconWrap}>
+              <Image
+                source={lockIcon}
+                style={styles.icon}
+                resizeMode="contain"
+              />
+            </View>
+            <TextInput
+              placeholder="Password"
+              secureTextEntry
+              style={styles.input}
+              underlineColorAndroid="transparent"
+            />
+          </View>
+      </View>
       </ImageBackground>
       </View>
     );
